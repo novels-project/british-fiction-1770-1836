8224ffd9426b623e9cbf8ea8f5c0e58e5fe074c1
Allen Riddell <abr@ariddell.org>
2015-02-23 18:55:23 -0500

    Add script and data to process 1830-1836

--- README.md
+++ README.md
@@ -11,15 +11,23 @@ The following files contain machine-readable versions of these sources:
 
 - `data/british-fiction-1800-1829.json`
 
-## Recreating the dataset
+## Recreating the dataset (1800-1829)
 
-1. Retrieve the index and detail pages from  `http://www.british-fiction.cf.ac.uk` using `scripts/download-british-fiction-1800-1836.py`.
-2. Process the downloaded pages with `scripts/process-british-fiction-1800-1829.py`.
-
-## Mirror of www.british-fiction.cf.ac.uk
+1.  Retrieve the index and detail pages from  `http://www.british-fiction.cf.ac.uk` using `scripts/download-british-fiction-1800-1836.py`.
+2.  Process the downloaded pages with `scripts/process-british-fiction-1800-1829.py`.
 
 The partial mirror of `www.british-fiction.cf.ac.uk` created by
 `downloaded-british-fiction-1800-1836.py` was retrieved January 30th, 2015.
+
+## Recreating the dataset (1830-1836)
+
+1.  Retrieve the file `1830-36.html` from `http://www.cf.ac.uk/encap/journals/corvey/1830s/1830-36.html` and
+    place it in `data/www.cf.ac.uk/`.
+2.  Process the html with `scripts/process-british-fiction-1830-1836.py`.
+
+A copy of `1830-36.html` is stored in this repository. The server at
+`http://www.cf.ac.uk/` reports its last modification date as: Tue, 22 May 2012
+12:40:26 GMT
 
 ## Correcting errors
 
@@ -30,6 +38,5 @@ parsing scripts.
 
 ## TODO
 
-- Add 1830-1836
-- Add "updates" if not already included (a small number)
+- Add "updates" from updates 1-6 that are not included (e.g., update 6 has a text by ANON with title De Courcy...)
 - Add 1770-99 (not yet trivially machine readable)
---
+++ scripts/process-british-fiction-1830-1836.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+
+import collections
+import json
+import logging
+import os
+import re
+
+import bs4
+
+BASE_DIR = os.path.abspath(__file__ + '/../../')
+INPUT_DIR = os.path.join(BASE_DIR, 'data', 'www.cf.ac.uk')
+OUTPUT_DIR = os.path.join(BASE_DIR, 'data')
+OUTPUT_FN = os.path.join(OUTPUT_DIR, 'british-fiction-1830-1836.json')
+
+if __name__ == '__main__':
+    index_fn = os.path.join(INPUT_DIR, '1830-36.html')
+    if not os.path.exists(index_fn):
+        msg = "Mirror of british-fiction.cf.ac.uk not found."
+        raise RuntimeError(msg)
+    html = open(index_fn, 'r', encoding='latin1').read()
+    # \xa0 is non-breaking space in Latin1 (ISO 8859-1)
+    html = html.replace(r'\xa0', ' ')
+    soup = bs4.BeautifulSoup(html)
+    title_ids = set()
+    records = []
+    for rec in soup('p', align="justify"):
+        record = collections.OrderedDict()
+        text = rec.text
+        html = str(rec)
+        title_id_matches = re.search(r'^\s*([0-9]{4}):\s*([0-9]+\s*[()a-z]*)', text)
+        if title_id_matches is None:
+            continue
+        title_id = ':'.join(title_id_matches.groups())
+        # strip all whitespace from title id
+        title_id = re.sub(r'\s+', '', title_id)
+        if title_id in title_ids:
+            raise RuntimeError("Found duplicate id {}".format(title_id))
+        title_ids.add(title_id)
+        record['id'] = title_id
+        record['year'] = int(title_id_matches.group(1))
+
+        # extract author
+        lines = html.split('<br/>')
+        line_cleaned = re.sub(r'\s+', ' ', bs4.BeautifulSoup(lines[0]).text)
+        author_match = re.search(r'[{[A-Z].*$', line_cleaned)
+        if author_match is None:
+            raise RuntimeError("Did not find author in title id {}".format(title_id))
+        record['author'] = author_match.group()
+
+        # extract title
+        line_cleaned = re.sub(r'\s+', ' ', bs4.BeautifulSoup(lines[1]).text)
+        if re.search(r'[A-Z]+', line_cleaned) is None:
+            raise RuntimeError("Did not find title in title id {}".format(title_id))
+
+        # extract publisher
+        # first remove initial closing tags
+        line = lines[2].replace(r'</', '<')
+        line_cleaned = re.sub(r'\s+', ' ', bs4.BeautifulSoup(line).text)
+        if re.search(r'18[0-9]+.*\.', line_cleaned) is None:
+            raise RuntimeError("Did not find full publisher in title id {}".format(title_id))
+        record['publisher'] = line_cleaned
+        records.append(record)
+
+    ######################################################################
+    # reorganize results, sorting by Garside ID
+    ######################################################################
+    records = collections.OrderedDict((record['id'], record) for record in records)
+
+    ######################################################################
+    # store results
+    ######################################################################
+    # rearrange results
+    with open(OUTPUT_FN, 'w', encoding='utf8') as f:
+        s = json.dumps(records, f, ensure_ascii=False, indent=2)
+        # for some reason 0x96 sneaks in to the HTML; it's invariably an en-dash, '–' \u2013
+        s = s.replace('\x96', '\u2013')
+        # for some reason 0x97 sneaks in to the HTML; it's invariably an em-dash, '–' \u2014
+        s = s.replace('\x97', '\u2014')
+        # for some reason 0x97 sneaks in to the HTML; it's an ellipsis \u2026
+        s = s.replace('\x85', '\u2026')
+        # for some reason 0x92 sneaks in to the HTML; it's ’ \u2019
+        s = s.replace('\x92', '\u2019')
+        # for some reason 0x91 sneaks in to the HTML; it's ‘ \u2018
+        s = s.replace('\x91', '\u2018')
+        f.write(s)
